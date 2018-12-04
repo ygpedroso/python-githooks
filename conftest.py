607fedcdf206e7c3c455eca2ce7895a3a400667d
ygpedroso <ygpedroso2503@gmail.com>
2018-12-04 14:33:11 -0500

    Create basic fixtures for testing

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+import pytest
+
+__BASE_DIR__ = os.path.join(os.environ["PWD"], 'tmp')
+__GITHOOKS_BASE_DIR__ = os.path.join(__BASE_DIR__, '.git/hooks')
+
+
+@pytest.fixture
+def workspace_without_git():
+    os.makedirs(__BASE_DIR__)
+    yield __BASE_DIR__
+    shutil.rmtree(__BASE_DIR__)
+
+
+@pytest.fixture
+def workspace_with_git():
+    os.makedirs(__GITHOOKS_BASE_DIR__)
+    yield __GITHOOKS_BASE_DIR__
+    shutil.rmtree(__BASE_DIR__)
