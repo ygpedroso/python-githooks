ea8b90bc0e82962feb95c8fb599ad9ca9be2b39c
ygpedroso <ygpedroso2503@gmail.com>
2018-12-04 11:05:17 -0500

    Create helper function for creating git-hooks based on a config file

--- python_hooks/helpers.py
+++ python_hooks/helpers.py
@@ -1,4 +1,8 @@
+import os
+import sys
+import stat
 from configparser import ConfigParser
+from .hook_template import hook_template
 
 
 def create_config_file(configfile_path):
@@ -7,3 +11,25 @@ def create_config_file(configfile_path):
     with open(configfile_path, 'w') as configfile:
         config.write(configfile)
 
+
+def create_git_hooks(configfile_path, githooks_dir):
+    config = ConfigParser()
+    if os.path.isfile(configfile_path):
+        config.read(configfile_path)
+        for section in config.sections():
+            hook = config[section]
+            if 'command' in hook:
+                command = hook['command']
+                githook_file = os.path.join(githooks_dir, section)
+                with open(githook_file, 'wb') as file:
+                    file.write(hook_template.format(command=command).encode())
+                    st = os.stat(githook_file)
+                    os.chmod(githook_file, st.st_mode | stat.S_IEXEC)
+                print('{} hook successfully created for running "{}"'.format(section, command))
+    else:
+        message = '''
+        Unable to find the ".githooks" configuration file.
+        Please, create it and try again.
+        '''
+        print(message)
+        sys.exit(1)
